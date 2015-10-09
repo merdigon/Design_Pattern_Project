176480473052520c3e4514fa3c7f9db98e3691ea
merdigon <szymon.nowak3848@gmail.com>
2015-10-10 00:32:29 +0200

    [S] Wstępna koncepcja :D

---
+++ WPServer/WPProjectServer/WPProjectServer/Logic/ClientLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPProjectServer.DatabaseObjects;
+using WPProjectServer.Logic.SubsystemLogic;
+
+namespace WPProjectServer.Logic
+{
+    public class ClientLogic : IClientLogic
+    {
+        List<ISubsystemLogic> ServerLogics;
+
+        public ClientLogic()
+        {
+            ServerLogics = new List<ISubsystemLogic>();
+            LoadLogics();
+        }
+
+        void LoadLogics()
+        {
+            ServerLogics.Add(new BookIOSubLogic());
+        }
+
+        public Book Return(Book bookToReturn)
+        {
+            foreach(ISubsystemLogic subLogic in ServerLogics)
+            {
+                if(subLogic is BookIOSubLogic)
+                {
+                    return ((BookIOSubLogic)subLogic).Return(bookToReturn);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ WPServer/WPProjectServer/WPProjectServer/Logic/SubsystemLogic/BookIOSubLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPProjectServer.DatabaseObjects;
+
+namespace WPProjectServer.Logic.SubsystemLogic
+{
+    public class BookIOSubLogic : ISubsystemLogic
+    {
+        public Book Return(Book book)
+        {
+            return book;
+        }
+    }
+}
